ff767089c59859090b6069f19fd7fff36fa5638f
naamiru <naamiruuu@gmail.com>
2025-09-18 17:26:25 +0900

    appear image viewer with fading

---
+++ EagleViewer/Services/ImageViewerManager.swift
@@ -0,0 +1,37 @@
+//
+//  ImageViewerManager.swift
+//  EagleViewer
+//
+//  Created on 2025/09/18
+//
+
+import SwiftUI
+
+class ImageViewerManager: ObservableObject {
+    @Published var isPresented = false
+
+    var item: Item?
+    var items: [Item]?
+    var dismiss: ((Item) -> Void)?
+
+    @Published var namespace: Namespace.ID
+
+    init(namespace: Namespace.ID) {
+        self.namespace = namespace
+    }
+
+    func show(item: Item, items: [Item], onDismiss: @escaping (Item) -> Void) {
+        self.item = item
+        self.items = items
+        dismiss = { value in
+            withAnimation(.easeInOut(duration: 0.2)) {
+                self.isPresented = false
+            }
+            onDismiss(value)
+        }
+
+        withAnimation(.easeInOut(duration: 0.2)) {
+            self.isPresented = true
+        }
+    }
+}
--- EagleViewer/Views/MainView.swift
+++ EagleViewer/Views/MainView.swift
@@ -15,6 +15,9 @@ struct MainView: View {
     @EnvironmentObject private var eventCenter: EventCenter
     @StateObject private var navigationManager = NavigationManager()
     @StateObject private var searchManager = SearchManager()
+    @StateObject private var imageViewerManager = ImageViewerManager(namespace: Namespace().wrappedValue)
+    @Namespace private var imageViewerNamespace
+
     @State private var libraryAccessTask: Task<Void, Error>?
 
     var body: some View {
@@ -34,10 +37,32 @@ struct MainView: View {
                         }
                     }
             }
+            .zIndex(0)
 
             VStack {
                 Spacer()
                 BottomBarView()
+            }
+            .zIndex(1)
+
+            if imageViewerManager.isPresented,
+               let item = imageViewerManager.item,
+               let items = imageViewerManager.items,
+               let dismiss = imageViewerManager.dismiss
+            {
+                Group {
+                    if ItemVideoView.isVideo(item: item) {
+                        ItemVideoView(item: item)
+                    } else {
+                        ImageDetailView(
+                            item: item,
+                            items: items.filter { !ItemVideoView.isVideo(item: $0) },
+                            dismiss: dismiss
+                        )
+                    }
+                }
+                .transition(.opacity)
+                .zIndex(2)
             }
         }
         .ignoresSafeArea(.keyboard)
@@ -48,6 +73,7 @@ struct MainView: View {
         }
         .environmentObject(navigationManager)
         .environmentObject(searchManager)
+        .environmentObject(imageViewerManager)
         .onChange(of: library, initial: true) { oldLibrary, newLibrary in
             if oldLibrary.id != newLibrary.id // library changed
                 || oldLibrary == newLibrary // inital call
@@ -76,6 +102,9 @@ struct MainView: View {
             if metadataImportManager.isImporting {
                 eventCenter.post(.importProgressChanged)
             }
+        }
+        .onChange(of: imageViewerNamespace, initial: true) {
+            imageViewerManager.namespace = imageViewerNamespace
         }
     }
 
