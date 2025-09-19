6bc4998b10b7196d9cecbeb79a429168f25475d9
naamiru <naamiruuu@gmail.com>
2025-09-19 21:39:29 +0900

    tag suggest

--- EagleViewer/Views/BottomBar/SearchBarView.swift
+++ EagleViewer/Views/BottomBar/SearchBarView.swift
@@ -15,10 +15,7 @@ struct SearchBarView: View {
 
     var body: some View {
         VStack {
-            HStack {
-                SearchSuggest()
-                Spacer()
-            }
+            SearchSuggestView()
 
             HStack {
                 HStack {
@@ -68,26 +65,136 @@ struct SearchBarView: View {
     }
 }
 
-struct SearchSuggest: View {
-    @State private var request = UsedTagsRequest()
+struct SearchSuggestView: View {
+    @State private var request = TagCountsRequest(libraryId: 0, destination: nil, searchText: "")
 
+    @Environment(\.library) private var library
     @EnvironmentObject private var searchManager: SearchManager
     @EnvironmentObject private var navigationManager: NavigationManager
 
     var body: some View {
-        VStack {
-            Text("tag name 1")
-            Text("tag name 2")
-        }
-        .padding()
-        .glassEffect(.regular, in: RoundedRectangle(cornerRadius: 12))
+        SearchSuggestInnerView(tagCountsRequest: $request)
+            .onChange(of: library.id, initial: true) {
+                request.libraryId = library.id
+            }
+            .onChange(of: navigationManager.path.last, initial: true) {
+                request.destination = navigationManager.path.last
+            }
+            .onChange(of: searchManager.debouncedSearchText, initial: true) {
+                request.searchText = searchManager.debouncedSearchText
+            }
     }
 }
 
-struct UsedTagsRequest: ValueObservationQueryable {
-    static var defaultValue: [String] { [] }
+struct SearchSuggestInnerView: View {
+    @Query<TagCountsRequest> private var tagCounts: [TagCount]
 
-    func fetch(_ db: Database) throws -> [String] {
-        return []
+    @EnvironmentObject private var searchManager: SearchManager
+
+    init(tagCountsRequest: Binding<TagCountsRequest>) {
+        _tagCounts = Query(tagCountsRequest)
+    }
+
+    var body: some View {
+        HStack(spacing: 0) {
+            if !tagCounts.isEmpty {
+                VStack(spacing: 0) {
+                    ForEach(tagCounts.enumerated(), id: \.element.tag) { index, tagCount in
+                        HStack {
+                            Image(systemName: "tag")
+                                .font(.caption)
+                            Text(tagCount.tag)
+                                .foregroundColor(.secondary)
+                                .lineLimit(1)
+                                .frame(maxWidth: 200)
+                                .fixedSize()
+                            Spacer()
+                            Text(String(tagCount.count))
+                                .foregroundColor(.secondary)
+                                .padding(.leading, 12)
+                        }
+                        .padding(.horizontal)
+                        .if(index == 0) { view in view.padding(.top) }
+                        .if(index != 0) { view in view.padding(.top, 6) }
+                        .if(index == tagCounts.count - 1) { view in view.padding(.bottom) }
+                        .if(index != tagCounts.count - 1) { view in view.padding(.bottom, 6) }
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            onTagTapped(tag: tagCount.tag)
+                        }
+                    }
+                }
+                .fixedSize(horizontal: true, vertical: false)
+                .glassEffect(.regular, in: RoundedRectangle(cornerRadius: 12))
+            }
+
+            Spacer()
+        }
+    }
+
+    private func onTagTapped(tag: String) {
+        let replaced = if searchManager.searchText.firstRange(of: /\s/) != nil {
+            searchManager.searchText.replacing(/(\s)\S*$/) { match in
+                match.1 + tag
+            }
+        } else {
+            tag
+        }
+        searchManager.searchText = replaced + " "
+    }
+}
+
+struct TagCountsRequest: ValueObservationQueryable {
+    var libraryId: Int64
+    var destination: NavigationDestination?
+    var searchText: String
+
+    static let queryableOptions = QueryableOptions.async
+
+    static var defaultValue: [TagCount] { [] }
+
+    func fetch(_ db: Database) throws -> [TagCount] {
+        let (itemSearchText, tagSearchText) = splitSearchText()
+        if tagSearchText.isEmpty {
+            return []
+        }
+
+        switch destination {
+        case nil: // Home
+            return []
+        case .folder(let id):
+            return try TagQuery.tagsInFolder(
+                libraryId: libraryId,
+                folderId: id.folderId,
+                itemSearchText: itemSearchText,
+                tagSearchText: tagSearchText,
+                limit: 4
+            ).fetchAll(db)
+        case .all, .random:
+            return try TagQuery.tagsInAll(
+                libraryId: libraryId,
+                itemSearchText: itemSearchText,
+                tagSearchText: tagSearchText,
+                limit: 4
+            ).fetchAll(db)
+        case .uncategorized:
+            return try TagQuery.tagsInUncategorized(
+                libraryId: libraryId,
+                itemSearchText: itemSearchText,
+                tagSearchText: tagSearchText,
+                limit: 4
+            ).fetchAll(db)
+        }
+    }
+
+    private func splitSearchText() -> (String, String) {
+        guard let lastSpaceIndex = searchText.lastIndex(where: { $0.isWhitespace }) else {
+            return ("", searchText)
+        }
+
+        return (
+            String(searchText[..<lastSpaceIndex]).trimmingCharacters(in: .whitespacesAndNewlines),
+            String(searchText[searchText.index(after: lastSpaceIndex)...]).trimmingCharacters(in: .whitespacesAndNewlines)
+        )
     }
 }
