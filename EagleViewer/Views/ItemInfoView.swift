69d9edef42322eef709896334b4a118d8f96b7f0
naamiru <naamiruuu@gmail.com>
2025-09-23 17:18:35 +0900

    refactor item info view

--- EagleViewer/Views/ItemInfoView.swift
+++ EagleViewer/Views/ItemInfoView.swift
@@ -10,23 +10,34 @@ import GRDBQuery
 import SwiftUI
 
 struct ItemInfoView: View {
-    let item: Item
-    @State private var storedItemRequest: StoredItemRequest
-    @State private var itemFoldersRequest: ItemFoldersRequest
+    @Query<StoredItemRequest> private var item: StoredItem
+    @Query<ItemFoldersRequest> private var folders: [Folder]
 
     @Environment(\.dismiss) private var dismiss
 
     init(item: Item) {
-        self.item = item
-        _storedItemRequest = State(initialValue: StoredItemRequest(id: item.id))
-        _itemFoldersRequest = State(initialValue: ItemFoldersRequest(id: item.id))
+        _item = Query(StoredItemRequest(id: item.id))
+        _folders = Query(ItemFoldersRequest(id: item.id))
     }
 
     var body: some View {
         NavigationStack {
             ScrollView {
-                ItemInfoInnerView(storedItemRequest: $storedItemRequest, itemFoldersRequest: $itemFoldersRequest)
-                    .padding(.horizontal)
+                VStack(alignment: .leading, spacing: 16) {
+                    ItemInfoOverview(item: item)
+
+                    ItemInfoFolders(folders: folders)
+
+                    if !item.tags.isEmpty {
+                        ItemInfoTags(tags: item.tags)
+                    }
+
+                    if !item.annotation.isEmpty {
+                        ItemInfoAnnotation(annotation: item.annotation)
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal)
             }
             .navigationBarTitleDisplayMode(.inline)
             .navigationTitle("Info")
@@ -43,135 +54,26 @@ struct ItemInfoView: View {
     }
 }
 
-struct ItemInfoInnerView: View {
-    @Query<StoredItemRequest> private var item: StoredItem
-    @Query<ItemFoldersRequest> private var folders: [Folder]
-
-    @Environment(\.dismiss) private var dismiss
-    @EnvironmentObject private var imageViewerManager: ImageViewerManager
-    @EnvironmentObject private var navigationManager: NavigationManager
-    @EnvironmentObject private var searchManager: SearchManager
-
-    init(storedItemRequest: Binding<StoredItemRequest>, itemFoldersRequest: Binding<ItemFoldersRequest>) {
-        _item = Query(storedItemRequest)
-        _folders = Query(itemFoldersRequest)
-    }
+struct ItemInfoOverview: View {
+    let item: StoredItem
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 16) {
-            VStack(alignment: .leading, spacing: 8) {
-                Text(verbatim: item.name)
-                    .bold()
-                Text(verbatim: "\(item.ext.uppercased()) · \(item.width) × \(item.height) · \(sizeText(item.size))")
-                    .font(.caption)
-                    .foregroundColor(.secondary)
+        VStack(alignment: .leading, spacing: 8) {
+            Text(verbatim: item.name)
+                .bold()
+            Text(verbatim: "\(item.ext.uppercased()) · \(item.width) × \(item.height) · \(sizeText(item.size))")
+                .font(.caption)
+                .foregroundColor(.secondary)
 
-                if item.star > 0 {
-                    HStack(spacing: 2) {
-                        ForEach(0 ..< 5, id: \.self) { i in
-                            Image(systemName: i < item.star ? "star.fill" : "star")
-                                .foregroundColor(.secondary)
-                        }
+            if item.star > 0 {
+                HStack(spacing: 2) {
+                    ForEach(0 ..< 5, id: \.self) { i in
+                        Image(systemName: i < item.star ? "star.fill" : "star")
+                            .foregroundColor(.secondary)
                     }
-                }
-            }
-
-            VStack(alignment: .leading, spacing: 8) {
-                Text("Folders")
-                    .font(.caption)
-                    .bold()
-                    .foregroundColor(.secondary)
-                FlowLayout(alignment: .leading) {
-                    if !folders.isEmpty {
-                        ForEach(folders, id: \.folderId) { folder in
-                            Button(action: {
-                                moveToFolder(folder)
-                            }) {
-                                Text(verbatim: folder.name)
-                                    .lineLimit(1)
-                                    .padding(.vertical, 5)
-                                    .padding(.horizontal, 12)
-                                    .foregroundColor(.primary.opacity(0.6))
-                                    .background(
-                                        RoundedRectangle(cornerRadius: 6)
-                                            .fill(.gray.opacity(0.1))
-                                    )
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 6)
-                                            .stroke(.gray.opacity(0.4))
-                                    )
-                            }
-                        }
-                    } else {
-                        Button(action: {
-                            moveToUncategorized()
-                        }) {
-                            Text("Uncategorized")
-                                .padding(.vertical, 5)
-                                .padding(.horizontal, 12)
-                                .foregroundColor(.primary.opacity(0.6))
-                                .background(
-                                    RoundedRectangle(cornerRadius: 6)
-                                        .fill(.gray.opacity(0.1))
-                                )
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 6)
-                                        .stroke(.gray.opacity(0.4))
-                                )
-                        }
-                    }
-                }
-            }
-
-            if !item.tags.isEmpty {
-                VStack(alignment: .leading, spacing: 8) {
-                    Text("Tags")
-                        .font(.caption)
-                        .bold()
-                        .foregroundColor(.secondary)
-                    FlowLayout(alignment: .leading) {
-                        ForEach(item.tags, id: \.self) { tag in
-                            Button(action: {
-                                moveToTag(tag)
-                            }) {
-                                Text(verbatim: tag)
-                                    .lineLimit(1)
-                                    .padding(.vertical, 5)
-                                    .padding(.horizontal, 12)
-                                    .foregroundColor(.primary.opacity(0.6))
-                                    .background(
-                                        RoundedRectangle(cornerRadius: 6)
-                                            .fill(.gray.opacity(0.1))
-                                    )
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 6)
-                                            .stroke(.gray.opacity(0.4))
-                                    )
-                            }
-                        }
-                    }
-                }
-            }
-
-            if !item.annotation.isEmpty {
-                VStack(alignment: .leading, spacing: 8) {
-                    Text("Notes")
-                        .font(.caption)
-                        .bold()
-                        .foregroundColor(.secondary)
-                    Text(verbatim: item.annotation)
-                        .textSelection(.enabled)
-                        .font(.caption)
-                        .foregroundColor(.primary.opacity(0.6))
-                        .padding()
-                        .background(
-                            RoundedRectangle(cornerRadius: 6)
-                                .fill(.gray.opacity(0.1))
-                        )
                 }
             }
         }
-        .frame(maxWidth: .infinity, alignment: .leading)
     }
 
     private func sizeText(_ sizeInBytes: Int) -> String {
@@ -191,6 +93,43 @@ struct ItemInfoInnerView: View {
             return "\(sizeInBytes)B"
         }
     }
+}
+
+struct ItemInfoFolders: View {
+    let folders: [Folder]
+
+    @Environment(\.dismiss) private var dismiss
+    @EnvironmentObject private var imageViewerManager: ImageViewerManager
+    @EnvironmentObject private var navigationManager: NavigationManager
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text("Folders")
+                .font(.caption)
+                .bold()
+                .foregroundColor(.secondary)
+            FlowLayout(alignment: .leading) {
+                if !folders.isEmpty {
+                    ForEach(folders, id: \.folderId) { folder in
+                        Button(action: {
+                            moveToFolder(folder)
+                        }) {
+                            Text(verbatim: folder.name)
+                                .lineLimit(1)
+                                .modifier(ItemInfoTag())
+                        }
+                    }
+                } else {
+                    Button(action: {
+                        moveToUncategorized()
+                    }) {
+                        Text("Uncategorized")
+                            .modifier(ItemInfoTag())
+                    }
+                }
+            }
+        }
+    }
 
     private func moveToFolder(_ folder: Folder) {
         dismiss()
@@ -205,6 +144,35 @@ struct ItemInfoInnerView: View {
         imageViewerManager.hide()
         DispatchQueue.main.async {
             navigationManager.path = [.uncategorized]
+        }
+    }
+}
+
+struct ItemInfoTags: View {
+    let tags: [String]
+
+    @Environment(\.dismiss) private var dismiss
+    @EnvironmentObject private var imageViewerManager: ImageViewerManager
+    @EnvironmentObject private var navigationManager: NavigationManager
+    @EnvironmentObject private var searchManager: SearchManager
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text("Tags")
+                .font(.caption)
+                .bold()
+                .foregroundColor(.secondary)
+            FlowLayout(alignment: .leading) {
+                ForEach(tags, id: \.self) { tag in
+                    Button(action: {
+                        moveToTag(tag)
+                    }) {
+                        Text(verbatim: tag)
+                            .lineLimit(1)
+                            .modifier(ItemInfoTag())
+                    }
+                }
+            }
         }
     }
 
@@ -223,8 +191,30 @@ struct ItemInfoInnerView: View {
     }
 }
 
+struct ItemInfoAnnotation: View {
+    let annotation: String
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text("Notes")
+                .font(.caption)
+                .bold()
+                .foregroundColor(.secondary)
+            Text(verbatim: annotation)
+                .textSelection(.enabled)
+                .font(.caption)
+                .foregroundColor(.primary.opacity(0.6))
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 6)
+                        .fill(.gray.opacity(0.1))
+                )
+        }
+    }
+}
+
 struct StoredItemRequest: ValueObservationQueryable {
-    var id: Item.ID
+    let id: Item.ID
 
     static var defaultValue: StoredItem {
         return StoredItem.empty
@@ -241,7 +231,7 @@ struct StoredItemRequest: ValueObservationQueryable {
 }
 
 struct ItemFoldersRequest: ValueObservationQueryable {
-    var id: Item.ID
+    let id: Item.ID
 
     static var defaultValue: [Folder] {
         return []
@@ -253,5 +243,22 @@ struct ItemFoldersRequest: ValueObservationQueryable {
             .joining(required: Folder.folderItems.filter(Column("itemId") == id.itemId))
             .order(Column("manualOrder"))
             .fetchAll(db)
+    }
+}
+
+struct ItemInfoTag: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding(.vertical, 5)
+            .padding(.horizontal, 12)
+            .foregroundColor(.primary.opacity(0.6))
+            .background(
+                RoundedRectangle(cornerRadius: 6)
+                    .fill(.gray.opacity(0.1))
+            )
+            .overlay(
+                RoundedRectangle(cornerRadius: 6)
+                    .stroke(.gray.opacity(0.4))
+            )
     }
 }
