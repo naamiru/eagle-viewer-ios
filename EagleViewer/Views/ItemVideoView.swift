b206603585874439fe4758edbcb7a0d7e8ee363f
naamiru <naamiruuu@gmail.com>
2025-09-30 11:23:04 +0900

    hide a video close button after a moment

--- EagleViewer/Views/ItemVideoView.swift
+++ EagleViewer/Views/ItemVideoView.swift
@@ -19,6 +19,9 @@ struct ItemVideoView: View {
     @State private var player: AVQueuePlayer?
     @State private var playerLooper: AVPlayerLooper?
     
+    @State private var showCloseButton = true
+    @State private var hideButtonTask: Task<Void, Never>?
+    
     @EnvironmentObject private var imageViewerManager: ImageViewerManager
     @EnvironmentObject private var libraryFolderManager: LibraryFolderManager
     
@@ -39,25 +42,31 @@ struct ItemVideoView: View {
         Group {
             if let player {
                 VideoPlayer(player: player) {
-                    VStack {
-                        HStack {
-                            Button(action: {
-                                dismiss()
-                            }) {
-                                Image(systemName: "chevron.down")
-                                    .foregroundColor(.white)
+                    if showCloseButton {
+                        VStack {
+                            HStack {
+                                Button(action: {
+                                    dismiss()
+                                }) {
+                                    Image(systemName: "chevron.down")
+                                        .foregroundColor(.white)
+                                }
+
+                                Spacer()
                             }
                             .padding()
-                            
+
                             Spacer()
                         }
-                        Spacer()
+                        .transition(.opacity) // not working
                     }
                 }
                 .ignoresSafeArea()
+                .simultaneousGesture(tapShowButtonGesture())
                 .simultaneousGesture(dragCloseGesture())
                 .onAppear {
                     player.play()
+                    scheduleButtonHide()
                 }
                 .onDisappear {
                     player.pause()
@@ -85,5 +94,37 @@ struct ItemVideoView: View {
                     dismiss()
                 }
             }
+    }
+    
+    private func tapShowButtonGesture() -> some Gesture {
+        TapGesture()
+            .onEnded { _ in
+                hideButtonTask?.cancel()
+
+                // show close button after 0.5sec
+                hideButtonTask = Task {
+                    try? await Task.sleep(nanoseconds: 500_000_000)
+                                
+                    if !Task.isCancelled {
+                        showCloseButton = true
+                        scheduleButtonHide()
+                    }
+                }
+            }
+    }
+    
+    private func scheduleButtonHide() {
+        hideButtonTask?.cancel()
+
+        // hide close button after 5sec
+        hideButtonTask = Task {
+            try? await Task.sleep(nanoseconds: 5_000_000_000)
+                
+            if !Task.isCancelled {
+                withAnimation {
+                    showCloseButton = false
+                }
+            }
+        }
     }
 }
